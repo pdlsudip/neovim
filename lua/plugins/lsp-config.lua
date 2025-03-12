1c397b81b45a5eb40e9c2f249a348540151896fe
Sudip Paudel <sudippaudel512@gmail.com>
2025-03-12 21:51:08 +0545

    removes tsserver added ts_ls

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -63,7 +63,7 @@ return {
 				on_attach = on_attach,
 			})
 			-- configure typescript server with plugin
-			lspconfig["tsserver"].setup({
+			lspconfig["ts_ls"].setup({
 				capabilities = capabilities,
 				on_attach = on_attach,
 				-- 16 gb
